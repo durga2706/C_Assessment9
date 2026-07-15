5dac589c99f5a33d871fd2a2850e50f6634a2d52
durga2706 <durkadevi0610@gmail.com>
2026-07-15 07:24:27 +0530

    Create c_problem1.c

---
+++ c_problem1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a[5];
+    int i, sum = 0;
+    printf("Enter 5 numbers:\n");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(i=0;i<5; i++)
+    {
+        sum =sum + a[i];
+    }
+    printf("%d", sum);
+    return 0;
+}
