211152819c0b49a21fbfafcc319b172a8b2c842a
durga2706 <durkadevi0610@gmail.com>
2026-07-16 15:51:16 +0530

    Create c_problem7.c

---
+++ c_problem7.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c, d, e, temp;
+    printf("Enter 5 numbers: ");
+    scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+    if (a < b)
+    {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    if (a<c)
+    {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    if (a<d)
+    {
+        temp = a;
+        a = d;
+        d = temp;
+    }
+    if (a < e)
+    {
+        temp = a;
+        a = e;
+        e = temp;
+    }
+    if (b < c)
+    {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+
+    if (b < d)
+    {
+        temp = b;
+        b = d;
+        d = temp;
+    }
+    if (b < e)
+    {
+        temp = b;
+        b = e;
+        e = temp;
+    }
+    if (c<d)
+    {
+        temp = c;
+        c = d;
+        d = temp;
+    }
+    if (c<e)
+    {
+        temp=c;
+        c=e;
+        e=temp;
+    }
+    if (d < e)
+    {
+        temp = d;
+        d = e;
+        e = temp;
+    }
+    printf("%d %d %d %d %d", a, b, c, d, e);
+    return 0;
+}
