cb3e10cd19ba80120b0ded3ede337c8e1e049933
durga2706 <durkadevi0610@gmail.com>
2026-07-16 16:02:43 +0530

    Create c_problem11.c

---
+++ c_problem11.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int a[5], b[5];
+    int i, n, rev, rem, sum = 0;
+    printf("Enter 5 numbers:\n");
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(i = 0; i < 5; i++)
+    {
+        n = a[i];
+        rev = 0;
+        while(n > 0)
+        {
+            rem = n % 10;
+            rev = rev *10 + rem;
+            n = n / 10;
+        }
+        b[i] = rev;
+        sum = sum + b[i];
+    }
+    printf("New array:\n");
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d ", b[i]);
+    }
+    printf("%d", sum);
+    return 0;
+}
