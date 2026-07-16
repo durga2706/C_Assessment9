b607565848d337d55d3ff6b9c980aaed20f78bb5
durga2706 <durkadevi0610@gmail.com>
2026-07-16 16:08:11 +0530

    Create c_problem13.c

---
+++ c_problem13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a[100];
+    int i = 0;
+    printf("Enter numbers:\n");
+    while(1)
+    {
+        scanf("%d", &a[i]);
+        if(a[i] == 0)
+        {
+            break;
+        }
+        i++;
+    }
+    if(i == 4 && a[0] == a[3])
+    {
+        printf("Success");
+    }
+    else
+    {
+        printf("Failure");
+    }
+    return 0;
+}
